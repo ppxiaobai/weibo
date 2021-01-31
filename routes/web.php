3d0b8fa6413e763faf199915ccc6abab671c2ad6
ppxiaobai <309997474@qq.com>
2021-01-31 11:13:41 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', 'StaticPagesController@home')->name("home");
 Route::get('/help', 'StaticPagesController@help')->name("help");
 //关于页
 Route::get('/about', 'StaticPagesController@about')->name("about");
+
+//用户注册
+Route::get("signup", "UsersController@create")->name("signup");
