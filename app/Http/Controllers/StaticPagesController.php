7fd82bae1bbfa75a413ee755128fcb2b9da7343c
ppxiaobai <309997474@qq.com>
2021-02-01 11:57:44 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,22 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StaticPagesController extends Controller
 {
-    //主页
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
-    //帮助页
     public function help()
     {
         return view('static_pages/help');
     }
 
-    //关于页
     public function about()
     {
         return view('static_pages/about');
