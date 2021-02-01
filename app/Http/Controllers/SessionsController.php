ce0e794e4e76f87ff04679f6e3f9e557a09833a5
ppxiaobai <309997474@qq.com>
2021-02-01 09:14:45 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -23,7 +23,8 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
@@ -36,5 +37,12 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash("success", "您已成功推出!");
         return redirect("login");
+    }
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
     }
 }
