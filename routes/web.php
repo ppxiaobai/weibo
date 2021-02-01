c1798dffeea32398898914ffb113dd7e591cd5a6
ppxiaobai <309997474@qq.com>
2021-02-01 11:14:50 +0000

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,10 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 //账号激活
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
